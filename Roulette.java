25a63396e453af35f280fb1c84655915edecf678
Gobi <gg357@kent.ac.uk>
2023-11-06 20:06:52 +0000

    defining variables

---
+++ Roulette.java
@@ -0,0 +1,30 @@
+public class Roulette {
+
+    private int[] straightUp, street, corner, topLine, doubleStreet, firstColumn, secondColumn, thirdColumn, firstDozen, secondDozen, thirdDozen, odd, even, red, black, oneToEighteen, nineteenToThirtysix;
+    private int zero;
+
+    public Roulette() {
+
+        int[] straightUp = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+        // int[] street =
+        // int[] corner = 
+        // int[] topLine = 
+        // int[] doubleStreet = 
+        // int[] firstColumn = 
+        // int[] secondColumn =
+        // int[] thirdColumn =
+        // int[] firstDozen = 
+        // int[] secondDozen =
+        // int[] thirdDozen =
+        int[] odd = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
+        int[] even = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36};
+        int[] red = {32, 19, 21, 25, 34, 27, 36, 30, 23, 5, 16, 1, 14, 9, 18, 7, 12, 3};
+        int[] black = {15, 4, 2, 17, 6, 13, 11, 8, 10, 24, 33, 20, 31, 22, 29, 28, 35, 26};
+        int[] oneToEighteen = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+        int[] nineteenToThirtysix = {19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
+        int zero = 0;
+
+    }
+
+
+}
